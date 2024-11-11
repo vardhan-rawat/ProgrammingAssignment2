fbc7a7c01821bee0ec42d1b3d0a5c6edbad31362
vardhan-rawat <vardhanrawat1@gmail.com>
2024-11-11 19:05:45 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to cache the inverse of a matrix.
+## The caching mechanism can optimize performance by avoiding repeated 
+## calculations of the inverse, which is particularly useful for large matrices.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
+## It contains functions to:
+## 1. Set the value of the matrix
+## 2. Get the value of the matrix
+## 3. Set the value of the inverse
+## 4. Get the value of the inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL  # Initialize the inverse as NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL  # Reset the inverse when the matrix is set
+    }
+    get <- function() x  # Return the matrix
+    setInverse <- function(inverse) inv <<- inverse  # Cache the inverse
+    getInverse <- function() inv  # Return the cached inverse
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix. If the inverse has already been calculated (and the matrix 
+## has not changed), then it retrieves the inverse from the cache.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {  # Check if the inverse is already cached
+        message("getting cached data")
+        return(inv)  # Return cached inverse
+    }
+    data <- x$get()  # Get the matrix from the special 'matrix' object
+    inv <- solve(data, ...)  # Calculate the inverse matrix
+    x$setInverse(inv)  # Cache the inverse matrix
+    inv  # Return the inverse matrix
 }
